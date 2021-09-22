a70e8db52d5aa9e93e83992172dfc6e09c023d1f
sarfarazshekh <sarfarazali.shekh@gmail.com>
2021-09-22 19:04:39 +0530

    add $http servie

--- js/my.js
+++ js/my.js
@@ -1,8 +1,19 @@
 var myApp = angular.module('myngapp', []);
-myApp.controller('myController', function ($scope) {
+myApp.controller('myController', function ($scope, $http) {
     $scope.title = "Home Index";
     $scope.message = "Hello This is my Controller";
     $scope.showMessage = function () {
         alert('this is a frist click');
     }
+    var onSuccees = function (data, status, header, config) {
+        $scope.data = data;
+    }
+    var onError = function (data, status, header, config) {
+        $scope.error = status;
+    }
+    $http.get(' http://localhost:3000/posts').then(function (response) {
+        var data = response.data;
+        $scope.user = data;
+    });
+
 });
