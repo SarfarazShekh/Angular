75185163019bedbb931af8544d0139bfce408db2
sarfarazshekh <sarfarazali.shekh@gmail.com>
2021-09-24 16:35:47 +0530

    add db file

--- js/myjs.js
+++ js/myjs.js
@@ -1,5 +1,27 @@
-var myapp=angular.module("myApp",[])
-myapp.controller("myFristCtrl",function($scope){
-    
+var myapp = angular.module("myApp", [])
+myapp.controller("myFristCtrl",['$scope',function ($scope) {
+    $scope.person = {};
+    $scope.role = [
+        {
+            value:"HR",
+            Id:1
+        },
+        {
+            value:"IT",
+            Id:2
+        },
+        {
+            value:"Admin",
+            Id:3
+        },
+        {
+            value:"Oprations",
+            Id:4
+        }
+    ];
+   
+    $scope.sendForm=function(){
+console.log(JSON.stringify($scope.person));
+    }
 
-});
+}]);
